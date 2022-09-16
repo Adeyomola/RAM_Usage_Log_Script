bbb8154c15376699d0dd397945428a82f0ba73ce
Adeyomola <kayzeel@yahoo.com>
2022-09-16 09:35:18 +0100

    added read me file

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+This is a simple bash script that records RAM Usage and sends the RAM Usage log for the day to a specific email address at midnight. 
+Attached to it is a cronjob that runs the script every hour, giving us an hourly RAM Usage log for the whole day.
--- ram.sh
+++ ram.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 #create memory log file
-touch system_memory_usage.log
+touch /home/vagrant/my_work/RAMUsageScript/system_memory_usage.log
 
 #get memory usage from free command
 MemoryUsage=`free --mega | awk 'NR==2{printf("- " "%.2f",$3*100/$2)}'`
@@ -10,14 +10,14 @@ MemoryUsage=`free --mega | awk 'NR==2{printf("- " "%.2f",$3*100/$2)}'`
 Time=$(date +%H:%M)
 
 #echo the value of MemoryUsage to the memory log file
-echo $MemoryUsage"%" "|" $Time >> system_memory_usage.log
+echo $MemoryUsage"%" "|" $Time >> /home/vagrant/my_work/RAMUsageScript/system_memory_usage.log
 
 #condition that sends email when it's midnight
 if [[ $(date +%H:%M) == "00:00" ]]
 then
-echo "Here's the memory usage log for the day" | mutt -a system_memory_usage.log -s "Memory Usage Log" -- kayzeel@yahoo.com
+echo "Here's the memory usage log for the day" | mutt -a /home/vagrant/my_work/RAMUsageScript/system_memory_usage.log -s "Memory Usage Log" -- kayzeel@yahoo.com
 
 #clears the memory log file
-> system_memory_usage.log
+> /home/vagrant/my_work/RAMUsageScript/system_memory_usage.log
 
 fi
